fd0e85f579b78c322ad561126b77673d0e807e45
Wout <wout@serverwars.net>
2025-12-13 20:49:23 +0100

    Leave queue on reloading config

--- src/main/kotlin/net/serverwars/sunsetPlugin/commands/config/CommandConfigReload.kt
+++ src/main/kotlin/net/serverwars/sunsetPlugin/commands/config/CommandConfigReload.kt
@@ -6,11 +6,19 @@ import io.papermc.paper.command.brigadier.CommandSourceStack
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import net.serverwars.sunsetPlugin.Main
+import net.serverwars.sunsetPlugin.domain.queue.exceptions.LeaveQueueException
+import net.serverwars.sunsetPlugin.domain.queue.services.QueueService
 import net.serverwars.sunsetPlugin.translations.TranslationManager
+import net.serverwars.sunsetPlugin.util.runAsync
 
 object CommandConfigReload {
 
     fun run(ctx: CommandContext<CommandSourceStack>): Int {
+        runAsync {
+           try {
+               QueueService.leaveQueue()
+           } catch (_: LeaveQueueException) {}
+        }
         Main.inst.reloadConfig()
         TranslationManager.loadTranslations()
         ctx.source.sender.sendMessage(Component.text("Config reloaded.", NamedTextColor.GRAY))
